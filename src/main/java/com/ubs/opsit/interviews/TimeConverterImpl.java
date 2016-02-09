bfcc288f69e1ae5eed12bdd614fa5c573899fd40
diwanmail <diwanmail@gmail.com>
2016-02-09 11:48:47 +0530

    Update TimeConverterImpl.java

--- src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
+++ src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
@@ -1,21 +1,22 @@
+package com.main;
+
 import java.time.LocalTime;
 import java.util.Arrays;
 
-
 public class TimeConverterImpl implements TimeConverter{
-	
-	private static final String LINE_SEPERATOR = "\n";
+
 	private final static char OFF = 'O';
 	private final static String SECOND_ROW_FORMAT = "Y";
 	private final static String HOUR_ROW_FORMAT = "RRRR";
 	private final static String MINUTE_TOP_ROW_FORMAT = "YYRYYRYYRYY";
 	private final static String MINUTE_BOTTOM_ROW_FORMAT = "YYYY";
+	private BerlinClock berlinClock;
 
 	@Override
 	public String convertTime(String aTime) {
 		LocalTime localTime = convertTimeStringToLocalTime(aTime);
-		BerlinClock berlinClock = buildBerlinClock(localTime);
-		return printBerlinClockRepresentation(berlinClock);
+		berlinClock = buildBerlinClock(localTime);
+		return berlinClock.toString();
 	}
 	
 	private LocalTime convertTimeStringToLocalTime(String aTime) {
@@ -33,35 +34,24 @@ public class TimeConverterImpl implements TimeConverter{
 	}
 	
 	private BerlinClock buildBerlinClock(LocalTime localTime) {
-		boolean secondRow = localTime.getSecond()%2 == 0;
-		int hoursTopRow = localTime.getHour()/5;
-		int hoursBottomRow = localTime.getHour()%5;
-		int minutesTopRow = localTime.getMinute()/5;
-		int minutesBottomRow = localTime.getMinute()%5;
+		String secondRow = (localTime.getSecond()%2 == 0) ? SECOND_ROW_FORMAT : Character.toString(OFF);
+		
+		String hoursTopRow = getBerlinClockRow(localTime.getHour()/5, HOUR_ROW_FORMAT);
+		String hoursBottomRow = getBerlinClockRow(localTime.getHour()%5, HOUR_ROW_FORMAT);
+		
+		String minutesTopRow = getBerlinClockRow(localTime.getMinute()/5, MINUTE_TOP_ROW_FORMAT);
+		String minutesBottomRow = getBerlinClockRow(localTime.getMinute()%5, MINUTE_BOTTOM_ROW_FORMAT);
+		
 		return new BerlinClock(secondRow, hoursTopRow, hoursBottomRow, minutesTopRow, minutesBottomRow);
 	}
 
-	private String printBerlinClockRepresentation(BerlinClock berlinClock) {
-		StringBuilder berlinClockBuilder = new StringBuilder();
-		
-		berlinClockBuilder.append(berlinClock.getSecondRow() ? SECOND_ROW_FORMAT : OFF);
-		berlinClockBuilder.append(LINE_SEPERATOR);
-		
-		printBerlinClockRow(berlinClockBuilder, berlinClock.getHoursTopRow(), HOUR_ROW_FORMAT);
-		
-		printBerlinClockRow(berlinClockBuilder, berlinClock.getHoursBottomRow(), HOUR_ROW_FORMAT);
-		
-		printBerlinClockRow(berlinClockBuilder, berlinClock.getMinutesTopRow(), MINUTE_TOP_ROW_FORMAT);
-		
-		printBerlinClockRow(berlinClockBuilder, berlinClock.getMinutesBottomRow(), MINUTE_BOTTOM_ROW_FORMAT);
-		
-		return berlinClockBuilder.toString();
-	}
-
-	private void printBerlinClockRow(StringBuilder berlinClockBuilder,int numBlinks, String berlinClockRowFormat) {
+	private String getBerlinClockRow(int numBlinkLamps, String berlinClockRowFormat) {
 		char[] row = berlinClockRowFormat.toCharArray();
-		Arrays.fill(row, numBlinks, row.length, OFF);
-		berlinClockBuilder.append(row);
-		berlinClockBuilder.append(LINE_SEPERATOR);
+		Arrays.fill(row, numBlinkLamps, row.length, OFF);
+		return String.valueOf(row);
+	}
+	
+	public BerlinClock getBerlinClock() {
+		return berlinClock;
 	}
 }
