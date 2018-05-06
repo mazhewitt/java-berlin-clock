947086faefec0cdb517bebe175f0cbae0e23e7bf
alokm13 <33947697+alokm13@users.noreply.github.com>
2018-05-06 23:40:19 +0530

    Update BerlinClockFixture.java

--- src/test/java/com/ubs/opsit/interviews/BerlinClockFixture.java
+++ src/test/java/com/ubs/opsit/interviews/BerlinClockFixture.java
@@ -4,8 +4,12 @@ import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.junit.Test;
 
+import java.time.format.DateTimeParseException;
+
 import static com.ubs.opsit.interviews.support.BehaviouralTestEmbedder.aBehaviouralTestRunner;
+import static org.assertj.core.api.Assertions.allOf;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Acceptance test class that uses the JBehave (Gerkin) syntax for writing stories.  You should not need to
@@ -13,8 +17,9 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class BerlinClockFixture {
 
-    private TimeConverter berlinClock;
+    private TimeConverter berlinClock = new TimeConverterImpl();
     private String theTime;
+    private Throwable exception;
 
     @Test
     public void berlinClockAcceptanceTests() throws Exception {
@@ -26,11 +31,21 @@ public class BerlinClockFixture {
 
     @When("the time is $time")
     public void whenTheTimeIs(String time) {
-        theTime = time;
+        try {
+            theTime = berlinClock.convertTime(time);
+        } catch (Throwable t) {
+            exception = t;
+        }
     }
 
     @Then("the clock should look like $")
     public void thenTheClockShouldLookLike(String theExpectedBerlinClockOutput) {
-        assertThat(berlinClock.convertTime(theTime)).isEqualTo(theExpectedBerlinClockOutput);
+        assertThat(theTime).isEqualTo(theExpectedBerlinClockOutput);
     }
+
+    @Then("DateTimeParseException happens")
+    public void thenDateTimeParseExceptionHappens() {
+        assertTrue(exception instanceof DateTimeParseException);
+    }
+
 }
